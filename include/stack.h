ab6ea8a0c57cfa7f24f634902b7781ea2b99ac95
NikAlex <alez90@yandex.ru>
2016-09-21 18:14:13 +0300

    Create stack.h

---
+++ include/stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK1_H_
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class stack
+{
+private:
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+public:
+	stack();
+	stack(const stack&);
+	~stack();
+	auto count() const noexcept->size_t;
+	auto push(T const &)->void;
+	auto pop()->T;
+	auto top() const->T&;
+	auto operator=(const stack&tmp)->stack&;
+	auto print()->void;
+};
+#endif
