1718bcdcb0a0182c9231474a71a513d3b1a89b04
NikAlex <alez90@yandex.ru>
2016-09-25 00:55:18 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,10 +1,9 @@
 #include "stack.h"
-
 template <typename T>//копирование и выделение памяти 
 auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
 	T *mass = new T[array_size_m];
 	copy(tmp,tmp+count_m,mass);
-	return mass; 
+	return mass;
 }
 
 
@@ -12,14 +11,14 @@ template <typename T>//освобождение памяти
 inline stack<T>::~stack()
 {
 	delete[] array_;
-}
+} 
 
 template <typename T>//конструктор по умолчанию
 inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
 
 template <typename T>//вставка элемента в стэк 
 inline auto stack<T>::push(T const &val)->void {
-	if (count_ == array_size_) {
+		if (count_ == array_size_) {
 			array_size_ *= 2;
 			T *tmp = mem_copy(count_,array_size_,array_);
 			delete[] array_;
@@ -33,7 +32,7 @@ inline auto stack<T>::push(T const &val)->void {
 
 
 template <typename T>//конструктор копирования
-inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(copy(tmp.count_, tmp.array_size, tmp.array_)) {}
+inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(copy(tmp.count_, tmp.array_size, tmp.array_)) {};
 
 template <typename T>//перегрузка оператора присваивания 
 inline auto stack<T>::operator=(const stack&tmp)->stack& {
@@ -46,6 +45,7 @@ inline auto stack<T>::operator=(const stack&tmp)->stack& {
 	}
 	return *this;
 }
+
 
 template <typename T>//возвращаем count_
 inline auto stack<T>::count() const noexcept->size_t {
